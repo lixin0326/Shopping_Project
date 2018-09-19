549b708bae2810feeb0aed9ed9460c358f3bef3a
lixin <2515081937@qq.com>
2018-09-19 23:25:40 +0800

    样式修改

--- apps/account/views.py
+++ apps/account/views.py
@@ -1,3 +1,19 @@
+from django.contrib.auth import authenticate, login, logout
+from django.db.models import Sum
 from django.shortcuts import render
+from apps.home.models import ShopCar
+from django.http import HttpResponse
 
-# Create your views here.
+
+def login_view(request):
+    if request.method == 'GET':
+        return render(request, 'login.html')
+    else:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user and user.is_active:
+            user.count = ShopCar.objects.filter(user_id=user.userprofile.uid).aggregate(Sum('number'))
+            login(request, user)
+        else:
+            return HttpResponse("不存在该用户!或该用户未激活!")
--- apps/car/views.py
+++ apps/car/views.py
@@ -1,6 +1,7 @@
 from django.db.models import F
 from django.shortcuts import render
 from apps.home.models import *
+from django.http import HttpResponse
 
 
 def add(request):
@@ -15,29 +16,30 @@ def add(request):
     try:
         shop_id = request.GET.get('shop_id')
         number = request.GET.get('number')
-        uid = request.user.id
-        car = ShopCar.objects.get(uid=uid, shop_id=shop_id, status=1)
-        if car:
+        uid = 1
+        cars = ShopCar.objects.filter(user_id=uid, shop_id=shop_id, status=1)
+        if cars:
+            car = cars.first()
             car.number = F('number') + number
             # car.number += number
             car.save(update_fields=['number'])
             # ShopCar.objects.select_for_update 高并发
         else:
-            car = ShopCar(uid=uid, number=number)
+            car = ShopCar(user_id=uid, shop_id=shop_id, number=number)
             car.save()
-    except:
+    except Exception as e:
         pass
 
-    return render(request)
+    return HttpResponse('11111')
 
 
 def delete(request):
-    return render(request)
+    pass
 
 
 def update_num(request):
-    return render(request)
+    pass
 
 
 def find(request):
-    return render(request)
+    pass
